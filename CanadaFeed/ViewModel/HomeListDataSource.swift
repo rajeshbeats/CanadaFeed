4c18ca201a0a112ebafd9ac755f3cf079d30b889
Rajesh <>
2019-08-12 17:33:34 +1000

    Home View basic update
    Add Home tableviewcell, view model, datasource files
    Storyboard update

---
+++ CanadaFeed/ViewModel/HomeListDataSource.swift
@@ -0,0 +1,15 @@
+//
+//  HomeListDataSource.swift
+//  CanadaFeed
+//
+//  Created by Ramachandrakurup, Rajesh on 12/8/19.
+//  Copyright © 2019 My Company. All rights reserved.
+//
+
+import Foundation
+
+protocol HomeListDataSourceProtocol {
+}
+
+extension HomeViewController: HomeListDataSourceProtocol {
+}
---
+++ CanadaFeed/ViewModel/HomeViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewModel.swift
+//  CanadaFeed
+//
+//  Created by Ramachandrakurup, Rajesh on 12/8/19.
+//  Copyright © 2019 My Company. All rights reserved.
+//
+
+import Foundation
