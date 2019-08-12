34537b8771a44221f4072d36bfbd1aef00809da3
Rajesh <>
2019-08-12 21:35:43 +1000

    Home feed service update
    API error handling
    View model and data source update

--- CanadaFeed/View/HomeViewController.swift
+++ CanadaFeed/View/HomeViewController.swift
@@ -8,84 +8,38 @@
 
 import UIKit
 
-class HomeViewController: UITableViewController {
+class HomeViewController: UIViewController {
+
+	@IBOutlet weak var tableView: UITableView!
+	let dataSource = HomeListDataSource()
+	lazy var  viewModel: HomeViewModelProtocol = {
+		return HomeViewModel(listDataSource: dataSource)
+	}()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-		FeedService.fetchFeed(index: 0, limit: 0)
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+		tableView.dataSource = dataSource
+		tableView.tableFooterView = UIView()
+		fetchFeed()
     }
 
-    // MARK: - Table view data source
+	func fetchFeed() {
+		viewModel.fetchFeedData(refresh: true) { [weak self] _, _ in
+			guard let this = self else {
+				return
+			}
+			this.tableView.reloadData()
+		}
+	}
+}
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+// MARK: - UITableViewDelegates
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+extension HomeViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return UITableView.automaticDimension
+	}
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+	}
 }
--- CanadaFeed/ViewModel/HomeViewModel.swift
+++ CanadaFeed/ViewModel/HomeViewModel.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+protocol HomeViewModelProtocol {
+
+	init(listDataSource: HomeListDataSource)
+	func fetchFeedData(refresh: Bool, completion:  @escaping FetchCompletion)
+}
+
+class HomeViewModel: HomeViewModelProtocol {
+	weak var dataSource: HomeListDataSource?
+
+	required init(listDataSource: HomeListDataSource) {
+		dataSource = listDataSource
+	}
+
+	func fetchFeedData(refresh: Bool, completion:  @escaping FetchCompletion) {
+
+		FeedService.fetchFeed(index: 0, limit: 0, refresh: refresh) { [weak self] list, error in
+			guard let this = self else {
+				return
+			}
+			guard let dataList = list?.rows else {
+				return
+			}
+			if refresh {
+				this.dataSource?.data = dataList
+			} else {
+				this.dataSource?.data.append(contentsOf: dataList)
+			}
+			completion(list, error)
+		}
+	}
+}
